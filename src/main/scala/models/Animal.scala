7eeff569ff137a59af1bdb184f76a041f8f369c0
El-PowerPtr <moiseselpower@gmail.com>
2025-05-26 11:47:56 -0400

    feature: Animal class has a checking constructor

--- src/main/scala/models/Animal.scala
+++ src/main/scala/models/Animal.scala
@@ -5,3 +5,19 @@ case class Animal(
     weight: Double,
     daysInShelter: Int
 )
+
+object Animal:
+  def apply(
+      name: String,
+      specie: String,
+      age: Int,
+      weight: Double,
+      daysInShelter: Int
+  ): Animal =
+    require(age >= 0, "La edad del animal no puede ser negativa")
+    require(
+      daysInShelter >= 0,
+      "La cantidad de días de estancia del animal no puede ser negativa"
+    )
+    require(weight > 0, "El peso del animal debe ser mayor que 0")
+    Animal(name, specie, age, weight, daysInShelter)
